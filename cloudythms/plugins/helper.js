3635b390b0abb6f3539e7d9ccb43eab54b248804
Petra <42890599+petracoding@users.noreply.github.com>
2019-07-29 18:01:18 +0200

    Update helper.js

--- cloudythms/plugins/helper.js
+++ cloudythms/plugins/helper.js
@@ -6,10 +6,36 @@
 // <script src="https://cdn.jsdelivr.net/gh/petracoding/tumblr/cloudythms/plugins/helper.js"></script>
 // requires jQuery!
 
+// ----------------------------------------------------
+
+let scrollToTopMs = 500;
+let scrollToTopBreakpoint = 20;
+
+// ----------------------------------------------------
+
+// Add classes to body
+
 $(document).ready(function() {
     $('body').addClass('-loaded');
 });
 
 $(window).bind('load', function() {
     $('body').addClass('-loaded-images');
+});
+
+// Scroll To Top
+
+$(function(){
+    $("#scrolltotop").click(function(){        
+        $("html, body").animate({scrollTop : 0}, scrollToTopMs);
+    });
+});
+
+$(window).scroll(function(){
+    if (document.body.scrollTop > scrollToTopBreakpoint
+        || document.documentElement.scrollTop > scrollToTopBreakpoint) {
+        $("#scrolltotop").css("opacity","1");
+    } else {
+        $("#scrolltotop").css("opacity","0");
+    }
 });
